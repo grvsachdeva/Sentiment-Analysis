c118c3fc99429e8cad431e2bfa350e79ada13f35
gauravano <sachdeva.gaurav1997@gmail.com>
2019-02-16 17:06:26 +0530

    fetching data from scrapping hub

--- medium/build/lib.linux-x86_64-2.7/medium/spiders/techradar.py
+++ medium/build/lib.linux-x86_64-2.7/medium/spiders/techradar.py
@@ -1,25 +1,24 @@
 # -*- coding: utf-8 -*-
 import scrapy
-import pdb
 
 
-class ArticlesSpider(scrapy.Spider):
-    name = 'articles'
-    allowed_domains = ['medium.com']
+class TechradarSpider(scrapy.Spider):
+    name = 'techradar'
+    allowed_domains = ['techradar.com']
 
     def __init__(self, *args, **kwargs):
-        super(ArticlesSpider, self).__init__(*args, **kwargs)
+        super(TechradarSpider, self).__init__(*args, **kwargs)
         self.start_urls = [kwargs.get('search_url')]
 
     def parse(self, response):
-        urls = response.css(
-            'div.postArticle-content>a[data-action="open-post"]::attr(href)').extract()
+        urls = response.css('div.listingResult>a::attr(href)').extract()
+        print(len(urls))
         for url in urls:
             yield(scrapy.Request(url=url, callback=self.parse_article))
 
     def parse_article(self, response):
-        title = response.css('h1::text').get()
-        content = response.css('.sectionLayout--insetColumn ::text').extract()
+        title = response.css('header>h1::text').extract_first()
+        content = response.css('div#article-body p::text').extract()
         article = ''.join(content)
         yield({
             'title': title,
