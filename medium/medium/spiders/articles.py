9fb2c1099663d21e3b07b34f113a3a7a330011b7
sanchittanwar7 <tanwarsanchit9762@gmail.com>
2019-02-16 12:17:50 +0530

    medium spider minor changes

--- medium/medium/spiders/articles.py
+++ medium/medium/spiders/articles.py
@@ -1,34 +1,27 @@
 # -*- coding: utf-8 -*-
-import scrapy, pdb
+import scrapy
+import pdb
+
 
 class ArticlesSpider(scrapy.Spider):
     name = 'articles'
     allowed_domains = ['medium.com']
-    
-    def __init__(self, *args, **kwargs): 
-      super(ArticlesSpider, self).__init__(*args, **kwargs) 
-      self.start_urls = [kwargs.get('search_url')]
+
+    def __init__(self, *args, **kwargs):
+        super(ArticlesSpider, self).__init__(*args, **kwargs)
+        self.start_urls = [kwargs.get('search_url')]
 
     def parse(self, response):
-        basOrNahi = False
         urls = response.css(
             'div.postArticle-content>a[data-action="open-post"]::attr(href)').extract()
-        print(len(urls))
-        ids = response.css(
-            'div.postArticle-content>a[data-action="open-post"]::attr(data-post-id)').extract()
         for url in urls:
             yield(scrapy.Request(url=url, callback=self.parse_article))
-        # if(not basOrNahi):
-        #     query = 'https://medium.com/search/posts?q=biometric%20security&count=10'
-        #     for id in ids:
-        #         query = query + '&ignore=' + id
-        #     basOrNahi = True
-        #     yield(scrapy.Request(url=query, callback=self.parse))
 
     def parse_article(self, response):
+        title = response.css('h1::text').get()
         content = response.css('.sectionLayout--insetColumn ::text').extract()
         article = ''.join(content)
         yield({
-            'title': content[0],
+            'title': title,
             'text': article
         })
