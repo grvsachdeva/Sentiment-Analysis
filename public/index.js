5a8bbecfecd4ecb73d763579ce585138f0052405
gauravano <sachdeva.gaurav1997@gmail.com>
2019-03-18 18:54:32 +0530

    all functions working

--- public/index.js
+++ public/index.js
@@ -1,99 +1,91 @@
-$(document).ready(function(){
-  $('.tabs').tabs();
+$(document).ready(function() {
+  $(".tabs").tabs();
 });
 
 function make_chart(component, val) {
-  console.log('comp',component,' ','value',val);
+  console.log("comp", component, " ", "value", val);
   // progressbar.js@1.0.0 version is used
   // Docs: http://progressbarjs.readthedocs.org/en/1.0.0/
   var bar = new ProgressBar.Circle(component, {
-    color: '#aaa',
+    color: "#aaa",
     // This has to be the same size as the maximum width to
     // prevent clipping
     strokeWidth: 4,
     trailWidth: 1,
-    easing: 'easeInOut',
+    easing: "easeInOut",
     duration: 1400,
     text: {
       autoStyleContainer: false
     },
-    from: { color: '#aaa', width: 2 },
-    to: { color: '#333', width: 4 },
+    from: { color: "#aaa", width: 2 },
+    to: { color: "#333", width: 4 },
     // Set default step function for all animate calls
     step: function(state, circle) {
-      circle.path.setAttribute('stroke', state.color);
-      circle.path.setAttribute('stroke-width', state.width);
+      circle.path.setAttribute("stroke", state.color);
+      circle.path.setAttribute("stroke-width", state.width);
 
       var value = Math.round(circle.value() * 100);
       circle.setText(value);
-
     }
   });
   bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
-  bar.text.style.fontSize = '2rem';
+  bar.text.style.fontSize = "2rem";
 
-  bar.animate(val);  // Number from 0.0 to 1.0
+  bar.animate(val); // Number from 0.0 to 1.0
 }
 
-function analyse_text(){
-  let test_text = document.getElementById('test_text').value;
+function analyse_text(textArray) {
+  console.log("TEXTARRAY---", textArray);
+  let test_text;
+  if (textArray == undefined) {
+    test_text = document.getElementById("test_text").value;
+  } else {
+    test_text = textArray;
+  }
 
-$.post('api/analyse/',{
-    test_text: test_text
-  },function (data) {
-    console.log(data);
-    $('.progressbar')[0].innerHTML = "";
-    $('.progressbar')[1].innerHTML = "";
-    $('.progressbar')[2].innerHTML = "";
-    $('.progressbar')[3].innerHTML = "";
+  $.post(
+    "api/analyse/",
+    {
+      test_text: test_text
+    },
+    function(data) {
+      console.log(data);
+      $(".progressbar")[0].innerHTML = "";
+      $(".progressbar")[1].innerHTML = "";
+      $(".progressbar")[2].innerHTML = "";
+      $(".progressbar")[3].innerHTML = "";
 
-    make_chart(chart1, data.SentimentScore.Positive);
-    var html = `<span class='prop_name'>Positive</span>`
-    $('#chart1').append(html);
-    make_chart(chart2, data.SentimentScore.Neutral);
-    html = `<span class='prop_name'>Neutral</span>`
-    $('#chart2').append(html);
-    make_chart(chart3, data.SentimentScore.Negative);
-    html = `<span class='prop_name'>Negative</span>`
-    $('#chart3').append(html);
-    make_chart(chart4, data.SentimentScore.Mixed);
-    html = `<span class='prop_name'>Mixed</span>`
-    $('#chart4').append(html);
-
-})
-
+      make_chart(chart1, data.SentimentScore.Positive);
+      var html = `<span class='prop_name'>Positive</span>`;
+      $("#chart1").append(html);
+      make_chart(chart2, data.SentimentScore.Neutral);
+      html = `<span class='prop_name'>Neutral</span>`;
+      $("#chart2").append(html);
+      make_chart(chart3, data.SentimentScore.Negative);
+      html = `<span class='prop_name'>Negative</span>`;
+      $("#chart3").append(html);
+      make_chart(chart4, data.SentimentScore.Mixed);
+      html = `<span class='prop_name'>Mixed</span>`;
+      $("#chart4").append(html);
+    }
+  );
 }
 
-
-function analyse_scrapy(){
-  let search_term = document.getElementById('search_term').value;
+function analyse_scrapy() {
+  let search_term = document.getElementById("search_term").value;
   let website = $("input[name='website']:checked").val();
 
-  console.log(search_term, " ", website );
-  
-$.post('api/analyse/scrapy/',{
-    search_term: search_term,
-    website: website
-  },function (data) {
-    console.log(data);
-    $('.progressbar')[0].innerHTML = "";
-    $('.progressbar')[1].innerHTML = "";
-    $('.progressbar')[2].innerHTML = "";
-    $('.progressbar')[3].innerHTML = "";
+  console.log(search_term, " ", website);
 
-    make_chart(chart1, data.SentimentScore.Positive);
-    var html = `<span class='prop_name'>Positive</span>`
-    $('#chart1').append(html);
-    make_chart(chart2, data.SentimentScore.Neutral);
-    html = `<span class='prop_name'>Neutral</span>`
-    $('#chart2').append(html);
-    make_chart(chart3, data.SentimentScore.Negative);
-    html = `<span class='prop_name'>Negative</span>`
-    $('#chart3').append(html);
-    make_chart(chart4, data.SentimentScore.Mixed);
-    html = `<span class='prop_name'>Mixed</span>`
-    $('#chart4').append(html);
-
-})
-
+  $.post(
+    "api/analyse/scrapy/",
+    {
+      search_term: search_term,
+      website: website
+    },
+    function(data) {
+      console.log("calling analyse------");
+      analyse_text(data);
+    }
+  );
 }
--- routes/api/analyse.js
+++ routes/api/analyse.js
@@ -1,6 +1,6 @@
-const route = require('express').Router()
-var axios = require('axios');
-var querystring = require('querystring');
+const route = require("express").Router();
+var axios = require("axios");
+var querystring = require("querystring");
 
 var AWS = require("aws-sdk");
 var comprehend = new AWS.Comprehend({
@@ -9,15 +9,12 @@ var comprehend = new AWS.Comprehend({
   region: "us-east-1"
 });
 
-
-route.post('/',(req,res) => {
-
+route.post("/", (req, res) => {
   console.log(req.body.test_text);
-  
 
   var params = {
     LanguageCode: "en",
-    TextList: [req.body.test_text]
+    TextList: req.body.test_text
   };
   comprehend.batchDetectSentiment(params, function(err, data) {
     if (err) {
@@ -28,69 +25,82 @@ route.post('/',(req,res) => {
       res.status(200).send(data.ResultList[0]);
     }
   });
-})
+});
 
-
-route.post('/scrapy',(req,res) => {
-
-  const scrapping_data = async () => {
-    scraped_data = fetch_scrapped_data(spider_name)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-      })
-      .catch(error => {
-        console.log(error)
-      })
-  }
-  
-  var encodedURI, spider_name = req.body.website;
-  if( spider_name === 'gizmodo')
-    encodedURI = "https://gizmodo.com/search?q=" + encodeURIComponent(req.body.search_term);
-  else if(spider_name === 'articles')
-    encodedURI = "https://medium.com/search?q=" + encodeURIComponent(req.body.search_term);
-  else if(spider_name === 'techradar')
-    encodedURI = "https://www.techradar.com/search?searchTerm=" + encodeURIComponent(req.body.search_term);
-  else
-    console.log("Invalid Selection");
+route.post("/scrapy", (req, res) => {
+  var encodedURI,
+    spider_name = req.body.website;
+  if (spider_name === "gizmodo")
+    encodedURI =
+      "https://gizmodo.com/search?q=" +
+      encodeURIComponent(req.body.search_term);
+  else if (spider_name === "articles")
+    encodedURI =
+      "https://medium.com/search?q=" + encodeURIComponent(req.body.search_term);
+  else if (spider_name === "techradar")
+    encodedURI =
+      "https://www.techradar.com/search?searchTerm=" +
+      encodeURIComponent(req.body.search_term);
+  else console.log("Invalid Selection");
 
   const config = {
     headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
+      "Content-Type": "application/x-www-form-urlencoded"
     }
-  }
+  };
 
   const req_params = {
-    project: '373618',
-    apikey: '722c6a6fe309462498c368ff58dad14e',
-    spider: spider_name, 
+    project: "373618",
+    apikey: "722c6a6fe309462498c368ff58dad14e",
+    spider: spider_name,
     fields: "title,text",
     include_headers: 1
-}
+  };
 
   const requestBody = querystring.stringify({
-      project: '373618',
-      apikey: '722c6a6fe309462498c368ff58dad14e',
-      spider: spider_name,
-      search_url: encodedURI 
-  })
-  
-axios.post('https://app.scrapinghub.com/api/run.json', requestBody, config)
-.then((res) => {
-  console.log("Request success: ",res);
- 
-try{
-  scrapped_data = axios.get('https://app.scrapinghub.com/api/items.json', {params: req_params})
-  console.log(scrapped_data);
-}catch(error){
-  console.log("Error: ", error);
-}
-}).catch((error) => {
-  console.log("Error: ", error);
-})
+    project: "373618",
+    apikey: "722c6a6fe309462498c368ff58dad14e",
+    spider: spider_name,
+    search_url: encodedURI
+  });
 
+  // axios.post('https://app.scrapinghub.com/api/run.json', requestBody, config)
+  // .then((result) => {
+  //   console.log("Request success: ",result);
+  //   setTimeout(function(){
 
-})
+  //       axios.get('https://app.scrapinghub.com/api/items.json', {params: req_params}).then((result) => {
+  //       console.log("DATAAA: ",JSON.stringify(result.data), "TYYYYPPPEE:", typeof(result.data),"\n-----------------------\n");
 
-exports = module.exports = route
+  //       for(x in result.data){
+  //         console.log("XXXXXXXXX: ",x);
+  //       }
+  //       }).catch((error) => {
+  //         console.log("error", error);
+  //       })
+  //   },40000);
+  // }).catch((error) => {
+  //   console.log("Error: ", error);
+  // })
+  let textArray = [];
+  axios
+    .get("https://app.scrapinghub.com/api/items.json", { params: req_params })
+    .then(result => {
+      // console.log("DATAAA: ", result, "TYYYYPPPEE:", typeof(result.length),"\n-----------------------\n");
+
+      for (x of result.data) {
+        len = x.text.length;
+        if (len >= 1 && len <= 4000) {
+          textArray.push(x.text);
+        } else if (len >= 1 && len > 4000) {
+          textArray.push(x.text.substring(0, 4000));
+        }
+      }
+      res.send(textArray);
+    })
+    .catch(error => {
+      console.log("error", error);
+    });
+});
+
+exports = module.exports = route;
